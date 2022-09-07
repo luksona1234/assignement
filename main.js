fecced9daefa8afa831aa8c8d8b70d80850483ba
Luka <lukakhachvani0311@gmail.com>
2022-09-08 02:06:01 +0400

    ok

---
+++ main.js
@@ -0,0 +1,75 @@
+console.log(window.document);
+
+let cont = document.querySelector('.container');
+
+cont.style.maxWidth = '1920px'
+cont.style.border = '3px solid black'
+
+let box = document.querySelector('.box');
+
+box.style.width = '100%'
+box.style.height = '200px'
+box.style.backgroundColor = 'purple'
+box.style.display = 'flex'
+box.style.justifyContent = 'space around'
+console.log(box);
+
+let box1 = document.querySelector('.box1');
+
+box1.style.width = '100%'
+box1.style.height = '600px'
+box1.style.backgroundColor = 'cyan'
+console.log(box1);
+
+let p2 = document.querySelector('.p2');
+
+p2.style.fontSize = '20px'
+p2.style.color = 'white'
+p2.style.marginTop = '9%'
+console.log(p2);
+
+let box3 = document.querySelector('.box3');
+
+box3.style.width = '350px'
+box3.style.height = '50px'
+box3.style.border = '3px solid black'
+box3.style.marginLeft = '345px'
+box3.style.marginTop = '9%'
+box3.style.display = 'flex'
+box3.style.justifyContent = 'space between'
+console.log(box3);
+
+let box4 = document.querySelector('.box4');
+
+box4.style.width = '40%'
+box4.style.height = '100%'
+box4.style.borderRadius = '100%'
+box4.style.backgroundColor = 'pink'
+box4.style.textAllign = 'center'
+console.log(box4);
+
+let box5 = document.querySelector('.box5');
+
+box5.style.width = '40%'
+box5.style.height = '100%'
+box5.style.borderRadius = '100%'
+box5.style.backgroundColor = 'pink'
+box5.style.marginLeft = '65px'
+console.log(box5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
